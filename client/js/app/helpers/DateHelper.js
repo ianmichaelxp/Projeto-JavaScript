ca59cc29168e3ac03bfd1c6668c7136080a3f265
Ian Michael <ianmichael.xp@gmail.com>
2020-05-01 22:13:45 -0300

    Adicionado validacao com expressao regular

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -8,6 +8,9 @@ class DateHelper {
         return (`${data.getDate()}/${(data.getMonth() + 1)}/${(data.getFullYear())}`)};
 
     static textoParaData(texto){
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
+            throw new Error('Formato de data inválido, formato aceito aaaa-mm-dd');
         return new Date(...texto
             .split('-')
             .map((item, indice) => item - indice % 2 ))
